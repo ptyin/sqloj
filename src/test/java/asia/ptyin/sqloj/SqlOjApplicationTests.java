80921dda5adb5bac06e3cd1d2add4a3f152a701b
ptyin <peteryin1604@gmail.com>
2021-11-17 00:49:41 +0800

    build(backend): switch backend impl from flask to spring

---
+++ src/test/java/asia/ptyin/sqloj/SqlOjApplicationTests.java
@@ -0,0 +1,13 @@
+package asia.ptyin.sqloj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlOjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
